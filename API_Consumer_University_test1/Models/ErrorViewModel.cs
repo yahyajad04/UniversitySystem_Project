3be0684b43a6b10abc5b7a1e456bb360f502ed52
yahyajad04 <yahyajad04@yahoo.com>
2025-09-19 00:50:59 +0300

    Remove old project folders from repo

--- API_Consumer_University_test1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace API_Consumer_University_test1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
