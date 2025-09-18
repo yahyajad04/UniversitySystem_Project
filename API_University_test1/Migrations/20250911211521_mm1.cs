57208ec3d492ad205bfebbf99f8a6ffc325659ea
yahyajad04 <yahyajad04@yahoo.com>
2025-09-19 00:59:34 +0300

    Convert API_University_test1 from submodule to normal folder

---
+++ API_University_test1/Migrations/20250911211521_mm1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_University_test1.Migrations
+{
+    /// <inheritdoc />
+    public partial class mm1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Course_Hours",
+                table: "Courses");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Course_Hours",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
